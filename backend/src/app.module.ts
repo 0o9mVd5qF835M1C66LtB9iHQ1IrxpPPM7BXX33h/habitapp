753c3ef2e8a5567d7763de559634c7342954f4da
oybekalimat <59618418+oybekalimat@users.noreply.github.com>
2022-04-26 01:46:45 +0500

    fix build scripts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { ServeStaticModule } from "@nestjs/serve-static";
 
 import { AuthModule } from "./auth/auth.module";
 import { AppController } from "./app.controller";
@@ -7,9 +8,14 @@ import { AppService } from "./app.service";
 import { HabitModule } from "./habit/habit.module";
 import { UserModule } from "./user/user.module";
 import { DatabaseModule } from "./database/database.module";
+import path from "path";
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, "../../frontend", "build"),
+      exclude: ["/api*"],
+    }),
     AuthModule,
     UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
